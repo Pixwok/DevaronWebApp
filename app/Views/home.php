cad5bd6b413a3148b9d65bc70680c9faeb144d5c
pixwok <dev@pixwok.fr>
2025-02-25 18:45:18 +0100

    Home Page

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,17 +4,21 @@
 </div>
 
 <main class="page-content">
-    <section>
-        <h2>Bienvenue sur Devaron</h2>
-        <p>Devaron est un serveur Minecraft Vanilla sur whitelist. Nous vous proposons de rejoindre notre communauté de joueurs passionnée par l'expérience vanilla de Minecraft.
-            Notre serveur offre une expérience survie 100% Vanilla avec 0 plugins et 0 mod qui modifient l'expérience du jeu de base.
-            Le jeux sur le serveur est totalement libre et fonctionne sur la confiance instauré par la communnauté. Enfin, le serveur ne sera pas supprimé ou reset puisque l'objectif est d'avoir un serveur qui dure dans le temps.  
-        </p>
-    </section>
-    <div class="container">
+    <h2>Bienvenue sur Devaron</h2>
+    <div class="info-container">
         <section class="info-box">
-            <h3>Privé</h3>
-            <p>Devaron est un serveur sans aucun plugin de protection. C'est pour cela que pour protéger la communauté déjà active, une candidature est requise pour rejoindre le serveur. Les candidatures sont ensuite soigneusement validé par les membres de la communauté et par le staff.</p>
+            <div>
+                <img class="image-section" src="/assets/img/spawn.webp" alt="Pourquoi Devaron">
+            </div>
+            <div>
+                <h3>Pourquoi Devaron ?</h3>
+                <p>
+                    Devaron est un serveur communautaire Minecraft Vanilla sur whitelist. Nous vous proposons de rejoindre notre communauté de joueurs passionnée par l'expérience vanilla de Minecraft. 
+                    Notre serveur offre une expérience survie 100% Vanilla avec 0 plugins et 0 mod qui modifient l'expérience du jeu de base. Le jeux sur le serveur est totalement libre et fonctionne sur la confiance instauré par la communnauté. 
+                    Enfin, le serveur ne sera pas supprimé ou reset puisque l'objectif est d'avoir un serveur qui dure dans le temps. 
+                </p>
+                <a class="join-btn" href="/join">Nous Rejoindre</a>
+            </div>
         </section>
         <section class="info-box">
             <h3>Vanilla</h3>
