d57903852bdff2b37974224c6decde17580b6f5f
pixwok <dev@pixwok.fr>
2025-02-27 18:10:27 +0100

    Home page content

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,7 +4,7 @@
 </div>
 
 <main class="page-content">
-    <h2>Bienvenue sur Devaron</h2>
+    <h2 class="title-page">Bienvenue sur Devaron</h2>
     <div class="info-container">
         <section class="info-box">
             <div>
@@ -12,18 +12,16 @@
             </div>
             <div>
                 <h3>Pourquoi Devaron ?</h3>
-                <p>
-                    Devaron est un serveur communautaire Minecraft Vanilla sur whitelist. Nous vous proposons de rejoindre notre communauté de joueurs passionnée par l'expérience vanilla de Minecraft. 
-                    Notre serveur offre une expérience survie 100% Vanilla avec 0 plugins et 0 mod qui modifient l'expérience du jeu de base. Le jeux sur le serveur est totalement libre et fonctionne sur la confiance instauré par la communnauté. 
-                    Enfin, le serveur ne sera pas supprimé ou reset puisque l'objectif est d'avoir un serveur qui dure dans le temps. 
-                </p>
+                <p>Devaron est un serveur communautaire Minecraft Vanilla sur whitelist. Nous vous proposons de rejoindre notre communauté de joueurs passionnée par l'expérience vanilla de Minecraft. </p>
+                <p>Le serveur est accessible aux versions Java de Minecraft (premium). Le jeux sur le serveur est totalement libre et fonctionne sur la confiance instauré par la communnauté. C'est pour cela  que nous demandons une candidature afin de sélectionner soigneusement nos joueurs.</p>
+                <p>Enfin, le serveur ne sera pas supprimé ou reset puisque l'objectif est d'avoir un serveur qui dure dans le temps. </p>
                 <a class="join-btn" href="/join">Nous Rejoindre</a>
             </div>
         </section>
         <section class="info-box">
             <div>
                 <h3>Vanilla</h3>
-                <p>Nous offrons une véritable survie vanilla, sans aucune modification du jeux de base. Aucun plugin ou mod inutile sont installés sur le serveur. Rien de tel qu'une survie classique mais en multijoueur.</p>
+                <p>Notre serveur offre une expérience survie 100% Vanilla avec 0 plugins et 0 mod qui modifient l'expérience du jeu de base. Rien de tel qu'une survie classique mais en multijoueur.</p>
             </div>
             <div>
                 <img class="image-section" src="/assets/img/spawn.webp" alt="Pourquoi Devaron">
@@ -36,6 +34,8 @@
             <div>
                 <h3>Communauté</h3>
                 <p>Devaron est une communauté de joueurs qui se retrouvent autour d'une passion commune le jeux vidéo et plus particulièrement Minecraft.</p>
+                <p>Il s'agit d'un lieux de partage autour de plusieurs passions.</p>
+                <a class="join-btn" href="https://discord.gg/KhP7fhvAE9">Rejoindre le Discord</a>
             </div>
         </section>
     </div>
