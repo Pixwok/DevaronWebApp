2877390c0be66bcc6efa4534cb7f29cba1e39b64
pixwok <dev@pixwok.fr>
2025-03-24 18:15:45 +0100

    Add Bootstrap

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -9,37 +9,34 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <!--CSS-->
-        <link rel="stylesheet" href="/assets/css/reset.css">
-        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/css/app.css">
         <!--JS-->
-        <script type="module" src="/assets/js/nav.js"></script>
+        <script type="module" src="/assets/js/bootstrap.bundle.min.js"></script>
         <title>Devaron.fr | Serveur Minecraft Vanilla</title>
     </head>
     <body>
-        <header>
-            <div id="root-nav">
-                <nav id="desktop-nav">
-                    <a href="/"><img src="/assets/img/devaron-logo.png" alt="logo devaron"></a>
-                    <ul id="desktop-navlink">
-                        <li><a href="/">Accueil</a></li>
-                        <li><a href="/join">Nous rejoindre</a></li>
-                        <li><a href="/dynmap">Dynmap</a></li>
-                        <li><a href="">Statistiques</a></li>
-                    </ul>
-                </nav>
-
-                <nav id="mobile-nav">
-                    <div id="burger-btn">
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                        <span class="bar"></span>
-                    </div>
-                    <ul id="mobile-navlink">
-                        <li><a href="/">Accueil</a></li>
-                        <li><a href="/join">Nous rejoindre</a></li>
-                        <li><a href="/dynmap">Dynmap</a></li>
-                        <li><a href="">Statistiques</a></li>
-                    </ul>
-                </nav>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Devaron</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Nous Rejoindre</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Dynmap</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link">Statistiques</a>
+                    </li>
+                </ul>
+                </div>
             </div>
-        </header>
+        </nav>
