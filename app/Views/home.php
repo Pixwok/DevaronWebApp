3239a6489dd8ce9475f24480a32ca98a559c165c
Pixwok <40206880+Pixwok@users.noreply.github.com>
2025-04-05 15:42:06 +0200

    Card home page

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,16 +1,60 @@
-<header id="header-home" class="container-fluid">
-    <h2>Devaron</h2>
-    <h1>Serveur Minecraft 100% Vanilla</h1>
+<header id="header-home" class="container-fluid text-center">
+    <div class="row align-items-center">
+        <div class="col">
+            <h2>Devaron</h2>
+            <h1>Serveur Minecraft 100% Vanilla</h1>
+        </div>
+    </div>    
 </header>
 
 <main class="container-lg">
-    <div class="row">
+    <div class="row align-items-center">
         <div class="col">
             <h2>Bienvenue sur Devaron</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam  pulvinar vitae nunc vel condimentum. Morbi vulputate a ligula vitae  lacinia. Sed aliquam quam sed nisl mollis, pellentesque sodales felis  sodales. Curabitur blandit tortor dignissim, consequat nulla vitae,  aliquet nisl. Donec eu enim in magna sollicitudin sodales in id lorem.  Integer velit augue, pellentesque sit amet facilisis non, eleifend non  lectus.</p>
         </div>
         <div class="col">
             <span><?=$status?></span>
+            <span><?=$onlinePlayers?> joueurs en ligne</span>
+            <a class="btn btn-primary" href="#">Nous rejoindre</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    Vanilla
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                    <p>A well-known quote, contained in a blockquote element.</p>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    Vanilla
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                    <p>A well-known quote, contained in a blockquote element.</p>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    Vanilla
+                </div>
+                <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                    <p>A well-known quote, contained in a blockquote element.</p>
+                    </blockquote>
+                </div>
+            </div>
         </div>
     </div>
     
