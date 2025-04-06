702e1d4a393cd30ab96515079a5caf17ea21a498
Pixwok <40206880+Pixwok@users.noreply.github.com>
2025-04-06 15:44:41 +0200

    Footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,4 +1,4 @@
-<footer class="py-3  border-top border-secondary-subtle">
+<footer class="footer-page py-3 border-top border-secondary-subtle">
     <div class="container-lg d-flex justify-content-between">
         <span class="footer-text">Copyright © 2025 - Devaron.fr</span>
         <span class="footer-text">Devaron est un serveur Minecraft non affilié à Mojang ou Microsoft</span>
