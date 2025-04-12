f6876fc019541ce76ca34bd6b8a0a3da42188d87
pixwok <dev@pixwok.fr>
2025-04-12 08:58:48 +0200

    Fix join page

--- app/Views/join.php
+++ app/Views/join.php
@@ -56,39 +56,7 @@
     </div>
     <div class="row my-5 text-center">
         <div class="col">
-            <a class="btn btn-primary btn-lg fw-semibold" href="https://discord.gg/KhP7fhvAE9">Rejoindre notre Discord</a>
+            <a class="btn btn-primary btn-lg fw-semibold" href="https://discord.gg/KhP7fhvAE9" target="_blank">Rejoindre notre Discord</a>
         </div>
     </div>
-    <!--<h1 class="title-page">Nous rejoindre</h1>
-    <section class="rules">
-        <h2>Règlement</h2>
-        <p>Devaron est un serveur basé sur la confiance entre les membres de la communauté. Afin d'assurer une bonne ambiance sur le serveur, ce dernier dispose de quelques règles.</p>
-        <ul class="rules-list">
-            <li>Licence Minecraft obligatoire ;</li>
-            <li>Bonne ambiance ;</li>
-            <li>Respecter les constructions des autres joueurs ;</li>
-            <li>Les mods de cheat sont interdit ;</li>
-            <li>Le grief est strictement interdit ;</li>
-            <li>Les bugs de duplication hors rail, carpet, sable, concrete sont interdit ;</li>
-        </ul>
-    </section>
-    <section class="member">
-        <h2>Devenir membres</h2>
-        <p>Vous souhaitez nous rejoindre ? Il vous suffit de suivre les étapes suivantes. Nous acceptons les candidatures en fonction de leur qualité, leur contenu et de l'avis des autres joueurs.</p>
-        <div class="list-state">
-            <div class="state">
-                <h3>Prendre connaissance des règles du serveur</h3>
-                <p>Prennez connaissance des règles du serveur et de son focntionnement. N'hésitez pas à poser des questions sur le Discord. </p>
-            </div>
-            <div class="state">
-                <h3>Candidature écrite</h3>
-                <p>Déposer votre candidature sur notre Discord. N'hésiter pas à vous inspirer des autres candidatures. Mais appliquez vous dans la rédaction de cette dernière. Soyez imaginatif ! Votre cadidature sur Devaron est votre billet d'entrée sur le serveur.</p>
-            </div>
-            <div class="state">
-                <h3>Bienvenue</h3>
-                <p>Si votre candidature est acceptée. Il ne vous restera plus qu'à vous connecter sur le serveur afin de débuter votre aventure.</p>
-            </div>
-        </div>
-        <a class="join-discord" href="https://discord.gg/KhP7fhvAE9">Rejoindre notre Discord</a>
-    </section>-->
 </main>
